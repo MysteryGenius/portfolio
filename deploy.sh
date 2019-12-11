73526ae6d6d98f9b0df26a39ce63ec2745085330
MysteryGenius <tanjianzhen3000@gmail.com>
2019-12-12 07:30:16 +0800

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# Deploying to https://MysteryGenius.github.io
+git push -f git@github.com:MysteryGenius/MysteryGenius.github.io.git master
+
+
+cd -
